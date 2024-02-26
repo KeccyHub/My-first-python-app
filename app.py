64fdd5913d14757938f942bd3789641c7b20ce4e
KeccyHub <okechukwukeccy@gmail.com>
2024-02-26 16:51:02 +0100

    app.y

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-print("Results")
-response = int(input())
-thilist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-nxt = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-for x in thilist[-5:-1]:
-    for y in nxt:
-        print(f'{x} - {y}')
-        scores = x - y
-        while response == scores:
-            print("You win")
-            break
-        else:
-            print("You loose")
+# my_list = [1,4,5,6,2,2,1,3]
+# my_list.append(7)
+# my_list.reverse()
+# my_list.remove(1)
+# print(my_list)
+# import random as r
+# random_password= ''.join(str(r.randint(0,9,)) for _ in range(10))
+# print(random_password)
