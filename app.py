adcd472946ac6f77d49875202cd9b5f7aa5cf0e6
KeccyHub <okechukwukeccy@gmail.com>
2024-02-26 15:52:57 +0100

    bank.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-birth_year = input('Birth Year: ')
-print(type(birth_year))
-age = 2024 - int(birth_year)
-print(type(age))
-print(age)
+print("Results")
+response = int(input())
+thilist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+nxt = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for x in thilist[-5:-1]:
+    for y in nxt:
+        print(f'{x} - {y}')
+        scores = x - y
+        while response == scores:
+            print("You win")
+            break
+        else:
+            print("You loose")
